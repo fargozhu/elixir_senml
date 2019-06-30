43f832facb20bc8236c32d0407f8335f8b69b0c0
Jaime Gomes <jaime.gomes@calipo.io>
2019-06-30 10:28:08 +0200

    ExCoveralls added to mix project

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ElixirSenml.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
     ]
   end
 
