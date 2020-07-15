6806fcf3880439d234968f92954165509b1683e8
Jaime Gomes <fargozhu@users.noreply.github.com>
2020-07-15 12:10:45 +0200

    Update elixir_senml.ex

--- lib/elixir_senml.ex
+++ lib/elixir_senml.ex
@@ -2,4 +2,6 @@ defmodule ElixirSenml do
   alias ElixirSenml.Resolver
 
   defdelegate resolve(payload), to: Resolver, as: :start_resolve
+  defdelegate unresolve(payload), to: Resolver, as: :stop_resolve
+  
 end
